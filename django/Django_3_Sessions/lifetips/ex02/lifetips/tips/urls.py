748a4edb17002ff797597e320b0d5f9f78377b19
Juan Urrutia Araolaza <jurrutia@c4r6s6.42urduliz.com>
2025-05-12 18:08:03 +0200

    Django_Sessions_def

--- django/Django_3_Sessions/lifetips/ex02/lifetips/tips/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='home'),         # Página principal: muestra tips y formulario si logueado
-    path('register/', views.register, name='register'),  # Registro de usuario
-    path('login/', views.login_view, name='login'),      # Login de usuario
-    path('logout/', views.logout_view, name='logout'),   # Logout de usuario
-]
--- django/Django_3_Sessions/lifetips/ex06/tips/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-class TipsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'tips'  # Debe coincidir con INSTALLED_APPS
-
-    def ready(self):
-        # Importa el archivo donde definiste la señal
-        import tips.signals  # Ajusta la ruta según tu estructura de proyecto
